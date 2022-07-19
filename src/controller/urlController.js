094e68157ee83786ce4cdf238d2c2af5c377792f
rahul <rahulbarnwalonlyu2@gmail.com>
2022-07-19 11:47:30 +0530

    updated

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,6 +1,5 @@
 const urlModel = require("../models/urlModel");
 const shortid = require("shortid");
-const validUrl = require("valid-url");
 
 // ===========> Create Url <=================
 const createUrl = async function (req, res) {
@@ -18,12 +17,18 @@ const createUrl = async function (req, res) {
         .status(400)
         .send({ status: false, message: "longUrl must be present" });
     }
-    if(!validUrl.isUri(longUrl)) {
-        return res.status(400).send({status: false, message: "Provide a valid url"})
-    }
-    let checkUrl = await urlModel.find({longUrl});
-    if(checkUrl.length != 0) {
-        return res.status(400).send({status: false, message: "Url already shorted"})
+    const regex =
+      /^([hH][tT][tT][pP]([sS])?:\/\/.)(www\.)?[-a-zA-Z0-9@:%.\+#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.#?&//=_]*$)/g;
+    if (typeof longUrl !== "string" || !regex.test(longUrl.trim()))
+      return res
+        .status(400)
+        .send({ status: false, message: "Please provide valid Url" });
+
+    let checkUrl = await urlModel.find({ longUrl });
+    if (checkUrl.length != 0) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Url already shorted" });
     }
 
     let baseUrl = "http://localhost:3000";
@@ -38,38 +43,42 @@ const createUrl = async function (req, res) {
 
     let savedUrl = await urlModel.create(url);
 
-    let finalUrl = await urlModel.findOne({_id: savedUrl._id}).select({__v:0, createdAt: 0, updatedAt:0, _id: 0});
+    let finalUrl = await urlModel
+      .findOne({ _id: savedUrl._id })
+      .select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 });
 
-    return res
-      .status(201)
-      .send({
-        status: true,
-        message: "shortUrl has been created successfully",
-        data: finalUrl,
-      });
+    return res.status(201).send({
+      status: true,
+      message: "shortUrl has been created successfully",
+      data: finalUrl,
+    });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
 
 // ============> Get Url Api <====================
-const getUrl = async function(req, res) {
-    try {
-        let shortUrl = req.params.urlCode;
+const getUrl = async function (req, res) {
+  try {
+    let shortUrl = req.params.urlCode;
 
-        if(!shortUrl) {
-            return res.status(400).send({ status:false, message: "urlCoder must be present"});
-        }
-        let getUrl = await urlModel.findOne({urlCode: shortUrl});
-        // console.log(getUrl);
-        if(getUrl) {
-            return res.redirect(getUrl.longUrl);
-        } else {
-            return res.status(404).send({ status: false, message: "Invalid urlCode"});
-        }
-    } catch(err) {
-        return res.status(500).send({ status: false, message: err.message})
+    if (!shortUrl) {
+      return res
+        .status(400)
+        .send({ status: false, message: "urlCoder must be present" });
     }
+    let getUrl = await urlModel.findOne({ urlCode: shortUrl });
+    // console.log(getUrl);
+    if (getUrl) {
+      return res.status(302).redirect(getUrl.longUrl);
+    } else {
+      return res
+        .status(400)
+        .send({ status: false, message: "Invalid urlCode" });
+    }
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
 };
 
 module.exports.createUrl = createUrl;
